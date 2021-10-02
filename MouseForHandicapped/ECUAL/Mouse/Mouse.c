7b1acc446240dc632181f90b5efe4bb6c5dfbade
Mahmoud Ayoub <mahmoudmagdyayoub99@gmail.com>
2021-10-03 00:42:45 +0200

    Added USB driver, modified config & makefile then Added Mouse driver

---
+++ MouseForHandicapped/ECUAL/Mouse/Mouse.c
@@ -0,0 +1,31 @@
+﻿/*
+ * Mouse.c
+ *
+ *  Author: Mahmoud Ayoub
+ */ 
+
+#include "Mouse.h"
+
+static report_t reportBuffer;
+
+void Mouse_init (void) {
+	/*  initialize INT0   */ 
+}
+void Mouse_MoveLeft(void) {
+	reportBuffer.dx = -1 ;
+}
+void Mouse_MoveRight(void) {
+	reportBuffer.dx = 1 ; 
+}
+void Mouse_MoveUp(void) {
+	reportBuffer.dy = -1 ; 
+}
+void Mouse_MoveDown(void) {
+	reportBuffer.dy = 1 ;
+}
+void Mouse_RightClick(void) {
+	reportBuffer.buttonMask = 1 ; 
+}
+void Mouse_LeftClick(void) {
+	reportBuffer.buttonMask = 2 ;
+}
---
+++ MouseForHandicapped/ECUAL/Mouse/Mouse.h
@@ -0,0 +1,29 @@
+﻿/*
+ * Mouse.h
+ *
+ *  Author: Mahmoud Ayoub
+ */ 
+
+
+#ifndef MOUSE_H_
+#define MOUSE_H_
+#include "../../MCAL/USB/usbdrv.h"
+
+typedef struct{
+	unsigned char buttonMask;
+	char    dx;
+	char    dy;
+	char    dWheel;
+}report_t;
+
+void Mouse_init (void) ; 
+void Mouse_MoveLeft(void) ; 
+void Mouse_MoveRight(void) ;	
+void Mouse_MoveUp(void) ;
+void Mouse_MoveDown(void) ;			
+void Mouse_RightClick(void) ;
+void Mouse_LeftClick(void);	
+
+
+
+#endif /* MOUSE_H_ */
