6c9c1f7a8f8a1cec3ef94ed1f39247fb5a145b80
Eslam Emara <emara.beats@gmail.com>
2021-10-14 21:32:18 +0200

    1 bug left

--- Mouse test/BN055/BN055.ino
+++ Mouse test/BN055/BN055.ino
@@ -27,6 +27,7 @@ s16_t current_reading[3] = {0,0,0};
 #define RIGHT_CLICK   5
 #define DOUBLE_LEFT_CLICK 6
 #define NOTHING     255
+#define STOP    8
 
 /* Angles */
 #define ROLL   2
@@ -43,9 +44,15 @@ s16_t current_reading[3] = {0,0,0};
 
 u8_t App_GetLargestAngle(s16_t value1,s16_t value2,s16_t value3)
 { // yaw roll pitch
+  
+  if (value1 > 180) value1-=360;
+  if (value2 > 180) value2-=360;
+  if (value3 > 180) value3-=360;
+  
   value1 = abs(value1);
   value2 = abs(value2);
   value3 = abs(value3);
+  
   if(value1 > value2)
   {
     if(value1 > value3)   { return 1; }
@@ -78,10 +85,6 @@ u8_t App_GetImuGradient(void)
     if (absolute_gradient[2] < 0 ){
      absolute_gradient[2] +=360;
   }
-  Serial.println(absolute_gradient[0]);
-  Serial.println(absolute_gradient[1]);
-  Serial.println(absolute_gradient[2]);
-  Serial.println("__________________________");
   //-- decide depending on calculations to which direction mouse should move
   // get the largest angle of them (sign isn't considered)
   u8_t angle_name = App_GetLargestAngle(absolute_gradient[0],absolute_gradient[1],absolute_gradient[2]);
@@ -90,26 +93,41 @@ u8_t App_GetImuGradient(void)
   switch (absolute_gradient[angle_name-1])
   {
     // 20 -> threshold
-    case 20 ... 40:
-    case 360-20 ... 360-40:
+    case 20 ... 35:
+    case 325 ... 340:
       cursor_speed = 1;
       break;
-    case 41 ... 60:
-    case 360-41 ... 360-60:
-      cursor_speed = 2;
-      break;
-    case 61 ... 89: // larger than 60
-    case 360-61 ... 360-89:
+    case 36 ... 50:
+    case 310 ... 324:
       cursor_speed = 3;
       break;
+    case 51 ... 75: // larger than 60
+    case 285 ... 309:
+      cursor_speed = 5;
+      break;
+    case 76 ... 90: // larger than 60
+    case 270 ... 284:
+      cursor_speed = 7;
+      break;
+    
+     
     default:
       cursor_speed = 0;
       break;
   }
-  
+  if (absolute_gradient[angle_name-1] < THRESHOLD_X && absolute_gradient[angle_name-1] > 360 - THRESHOLD_X){
+    angle_name = STOP;
+  }
   // take action on mouse according to angle
   switch(angle_name)
   {
+    case STOP:
+    //-- no head motion 
+    RIGHT_CLICK_FLAG = ENABLED;
+    LEFT_CLICK_FLAG = ENABLED;
+    DL_FLAG = ENABLED;
+    return NOTHING;
+    break;
     // head is turned around X-axis (RIGHT_CLICK, LEFT_CLICK, DOUBLE_LEFT_CLICK)
     case ROLL:
       // angle > threshold?
@@ -125,27 +143,27 @@ u8_t App_GetImuGradient(void)
         return RIGHT_CLICK;
         
       }
-      // angle < 360-threshold?
-      else if ( (absolute_gradient[angle_name-1]) <= (360-THRESHOLD_X) && (LEFT_CLICK_FLAG || DL_FLAG) )
+        
+      // angle in range [320:360] -> left click
+      else if ((absolute_gradient[angle_name-1] > 360-(THRESHOLD_X+40)) && (absolute_gradient[angle_name-1] < 360 - THRESHOLD_X) && LEFT_CLICK_FLAG)
       {
-
         // enable right click
         RIGHT_CLICK_FLAG = ENABLED;
-        // angle in range [320:360] -> left click
-        if (absolute_gradient[angle_name-1] > 360-(THRESHOLD_X+20))
-        {
-          // disable flag to stop sending this signal multiple times
-          LEFT_CLICK_FLAG = DISABLED;
-          // enable double left flag
-          DL_FLAG = ENABLED;
-           return LEFT_CLICK;
-        }
-        // angle in range [180:320] -> double left click
-        else if(absolute_gradient[angle_name-1] > 180)
-          DL_FLAG = DISABLED;
-          LEFT_CLICK_FLAG = ENABLED;
-          return DOUBLE_LEFT_CLICK;
+        // disable flag to stop sending this signal multiple times
+        LEFT_CLICK_FLAG = DISABLED;
+        // enable double left flag
+        DL_FLAG = ENABLED;
+         return LEFT_CLICK;
       }
+      // angle in range [180:320] -> double left click
+      else if((absolute_gradient[angle_name-1] > 180) && (absolute_gradient[angle_name-1] < 300) && DL_FLAG )
+      {
+        DL_FLAG = DISABLED;
+        LEFT_CLICK_FLAG = DISABLED;
+        RIGHT_CLICK_FLAG = ENABLED;
+        return DOUBLE_LEFT_CLICK;
+      }
+      return NOTHING;
       break;
     // head is turned around Y-axis (UP, DOWN)
     case PITCH:
@@ -154,28 +172,28 @@ u8_t App_GetImuGradient(void)
       if(absolute_gradient[angle_name-1] >= THRESHOLD_Y && absolute_gradient[angle_name-1] < 180)
       { RIGHT_CLICK_FLAG = ENABLED; LEFT_CLICK_FLAG = ENABLED; DL_FLAG = ENABLED; return UP; }
       // angle range [180:360]
-      else if ( absolute_gradient[angle_name-1] > 180 && absolute_gradient[angle_name-1] < 360 )
+      else if ( absolute_gradient[angle_name-1] > 180 && absolute_gradient[angle_name-1] < (360 - THRESHOLD_Y) )
       { RIGHT_CLICK_FLAG = ENABLED; LEFT_CLICK_FLAG = ENABLED; DL_FLAG = ENABLED; return DOWN; }
+      return NOTHING;
       break;
     // head is turned around Z-axis (Left, Right)
     case YAW:
       // enable flags so the signal can be sent again
       if(absolute_gradient[angle_name-1] >= THRESHOLD_Z && absolute_gradient[angle_name-1] < 180)
       {RIGHT_CLICK_FLAG = ENABLED; LEFT_CLICK_FLAG = ENABLED; DL_FLAG = ENABLED; return RIGHT; }
-      else if ( absolute_gradient[angle_name-1] > 180 && absolute_gradient[angle_name-1] < 360 )
+      else if ( absolute_gradient[angle_name-1] > 180 && absolute_gradient[angle_name-1] < (360 - THRESHOLD_Z) )
       {RIGHT_CLICK_FLAG = ENABLED; LEFT_CLICK_FLAG = ENABLED; DL_FLAG = ENABLED; return LEFT; }
+      return NOTHING;
       break;
+
   }
   //-- no head motion 
-  RIGHT_CLICK_FLAG = ENABLED;
-  LEFT_CLICK_FLAG = ENABLED;
-  DL_FLAG = ENABLED;
   return NOTHING;
 }
 
 
 void App_OrderMouse(u8_t order)
-{/*
+{
   switch(order)
   {
     // cursor speed is u16_t
@@ -183,13 +201,13 @@ void App_OrderMouse(u8_t order)
     case RIGHT:       Serial.print("RIGHT"); Serial.println(cursor_speed);   break;
     case UP:          Serial.print("UP"); Serial.println(cursor_speed);  break;
     case DOWN:        Serial.print("DOWN"); Serial.println(cursor_speed);  break;
-    case LEFT_CLICK:  Serial.println("LCLICK");   break;
-    case RIGHT_CLICK: Serial.println("RCLICK");   break;
+    case LEFT_CLICK:  Serial.println("LCLICK");  break;
+    case RIGHT_CLICK: Serial.println("RCLICK");  break;
     case DOUBLE_LEFT_CLICK:
               Serial.println("DLCLICK");
               break;
     case NOTHING:     Serial.println("STOP");   break;
-  }*/
+  }
 }
 
 u8_t App_SetReference(){
@@ -206,7 +224,7 @@ u8_t App_SetReference(){
 void App_Init()
 {
   pinMode(8, INPUT);  
-  Serial.begin(9600);
+  Serial.begin(4800);
   if (BNO055_Init() == BNO055_SUCCESS){
     Serial.println("BNO055 init success");
   }
--- Mouse test/MouseMovement.py
+++ Mouse test/MouseMovement.py
@@ -7,41 +7,40 @@ import time
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = True
 
-arduino = serial.Serial('COM3', 9600)
+arduino = serial.Serial('COM3', 4800)
 print("Arduino connected")
 time.sleep(2)
-def get_digits1(text):
+def get_digits(text):
     c = ""
     for i in text:
         if i.isdigit():
             c += i
     return c
 
-def get_digits(text):
-    return filter(str.isdigit, text)
-
 while(True):
+    try:
+        order_data = arduino.readline()
+        order_data = order_data.decode('UTF-8')
+        print(order_data)
 
-    order_data = arduino.readline()
-    order_data = order_data.decode('UTF-8')
-    print(order_data)
+        if ("LEFT" in order_data):
+            pyautogui.moveRel(-1*int(get_digits(order_data)), 0, duration=0)
+        elif ("RIGHT" in order_data):
+            pyautogui.moveRel(int(get_digits(order_data)), 0, duration=0)
+        elif ("UP" in order_data):
+            pyautogui.moveRel(0, -1*int(get_digits(order_data)), duration=0)
+        elif ("DOWN" in order_data):
+            pyautogui.moveRel(0, int(get_digits(order_data)), duration=0)
+        elif ("LCLICK" in order_data):
+            pyautogui.click(pyautogui.position())
+        elif ("RCLICK" in order_data):
+            pyautogui.click(button='right')
 
-    if ("LEFT" in order_data):
-        pyautogui.moveRel(-1*int(get_digits1(order_data)), 0, duration=0)
-    elif ("RIGHT" in order_data):
-        pyautogui.moveRel(int(get_digits1(order_data)), 0, duration=0)
-    elif ("UP" in order_data):
-        pyautogui.moveRel(0, -1*int(get_digits1(order_data)), duration=0)
-    elif ("DOWN" in order_data):
-        pyautogui.moveRel(0, int(get_digits1(order_data)), duration=0)
-    elif ("LCLICK" in order_data):
-        pyautogui.click(pyautogui.position())
-    elif ("RCLICK" in order_data):
-        pyautogui.click(button='right')
-
-    elif ("DLCLICK" in order_data):
-        pyautogui.click(clicks=2)
-        
-    else:
+        elif ("DLCLICK" in order_data):
+            pyautogui.click(clicks=2)
+            
+        else:
+            pass
+    except:
         pass
 
